d4216d98d4e7f19a1ea3909217076f6616684464
udomsakr66-debug <udomsakr66@nu.ac.th>
2025-12-26 09:44:11 +0700

    1

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -3,7 +3,7 @@ module.exports = {
     db: {
         database: process.env.DB_NAME || 'nvWebblogDb',
         user: process.env.DB_User || 'root',
-        password: process.env.DB_PASS || '',
+        password: process.env.DB_PASS || 'root',
         options: {
             dialect: process.env.DIALECT || 'sqlite',
             storage: './nvwebblog-db.sqlite'
---
+++ server/src/models/User.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, DataTypes) => {
+    // กำหนดโครงสร้างตาราง User
+    const User = sequelize.define('User', {
+        email: {
+            type: DataTypes.STRING,
+            unique: true // อีเมล์ต้องไม่ซ้ำ
+        },
+        password: DataTypes.STRING,
+        name: DataTypes.STRING,
+        lastname: DataTypes.STRING,
+        status: DataTypes.STRING,
+        type: DataTypes.STRING
+    })
+    return User
+}
